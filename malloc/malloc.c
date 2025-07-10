1920ccdad4228d837f25bd26fc1ce09500335f88
mutsumiyamaguchi <tayomuwa2020@gmail.com>
2025-07-10 13:52:07 +0900

    add right-merge

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -40,6 +40,8 @@ typedef struct my_heap_t {
 //
 // my_heap_t my_heap;
 my_heap_t my_heap[4];
+// heapの数を8に変更して実行してみたけれど変わらなかった。むしろ探索場所が増える分、実行時間が長くなった
+// my_heap_t my_heap[8];
 
 // サイズに応じてindexを返す関数
 int get_index(size_t size) {
@@ -52,6 +54,11 @@ int get_index(size_t size) {
     index = 3;
   }
   return index;
+  //  // それ以上であれば7にする
+  // if (index > 7){
+  //   index = 3;
+  // }
+ 
 }
 
 //
@@ -113,6 +120,11 @@ void my_initialize() {
     my_heap[i].dummy.size = 0;
     my_heap[i].dummy.next = NULL;
   }
+  // for (int i = 0; i < 8; ++i) {
+  //   my_heap[i].free_head = &my_heap[i].dummy;
+  //   my_heap[i].dummy.size = 0;
+  //   my_heap[i].dummy.next = NULL;
+  // }
 }
 
 
@@ -244,6 +256,30 @@ void *my_malloc(size_t size) {
       break;
     }
   }
+  // for(int i = index;i < 8;i++){
+
+  //   // 見つからなかった時のために、index以上のリストを探索できるようにする
+  //   my_metadata_t *metadata = my_heap[i].free_head;
+  //   my_metadata_t *prev = NULL;
+
+  //   while(metadata){
+  //   // もし欲しいサイズよりも大きいサイズのものを見つけたら
+  //     if (metadata->size >= size){
+  //     // bestfitが存在しなければそれを格納、またはbestfitの現在格納されているサイズよりも小さいものを見つけたら更新
+  //       if(bestfit == NULL || (bestfit -> size) > (metadata -> size) ){
+  //         bestfit = metadata;
+  //         bestfit_prev = prev;
+  //       }
+  //     }
+  //     prev = metadata;
+  //     metadata = metadata->next;
+  //   }
+
+  //   // もしbestfitに該当するものが見つかったらroopを抜ける
+  //   if(bestfit != NULL){
+  //     break;
+  //   }
+  // }
   
   // now, metadata points to the first free slot
   // and prev is the previous entry.
@@ -312,9 +348,56 @@ void my_free(void *ptr) {
   // ... | metadata | object | ...
   //     ^          ^
   //     metadata   ptr
+
+  // // 現在のコード
+  // // メタデータが格納されているポインタを取得
+  // my_metadata_t *metadata = (my_metadata_t *)ptr - 1;
+  // // Add the free slot to the free list.
+  // // その容量分メモリを解放
+  // my_add_to_free_list(metadata);
+  
+  // 右側をmergeしたい
+  // メタデータが格納されているポインタを取得
   my_metadata_t *metadata = (my_metadata_t *)ptr - 1;
-  // Add the free slot to the free list.
+
+  // 次のアドレスを参照して、その中身がfreelistに存在していたら結合するという処理を加える
+  // TODO 
+  // 下のコードだとmetadataの型が(my_metadata_t)型なので、(char *)metadata + (sizeof(my_metadata_t) + metadata->size) * sizeof(my_metadata_t)
+  // と同じになってしまうらしい
+  // my_metadata_t *metadata_next = (my_metadata_t *) metadata + sizeof(my_matadata_t) + metadata -> size ;
+  // 正しいコードは以下の通りcastする必要がある
+  my_metadata_t *metadata_next = (my_metadata_t *)((char *)metadata + sizeof(my_metadata_t) + metadata->size);
+
+  // 右隣のアドレスがfreelistに含まれているか探索する
+  // バッファを用意
+  my_metadata_t *buf = NULL;
+  // sizeからfreelistとして含まれている可能性のあるindexを取得
+  int index = get_index(metadata_next->size);
+  my_metadata_t *nowlist = my_heap[index].free_head;
+
+  while (nowlist){
+
+    // もしmetadata_next（右隣）がfreelistに含まれていたら、
+    if(nowlist == metadata_next){
+
+      // 一度削除して
+      // removeの引数は現在のアドレスから一つ前のアドレス
+      my_remove_from_free_list(nowlist,buf);
+      // metadataの大きさをmetadataが格納されているメモリ+データを格納することができるメモリのサイズ分増やす
+      metadata->size += sizeof(my_metadata_t) + nowlist->size;
+    }
+
+    // 一つ前の情報を現在探索中のアドレスに更新
+    buf = nowlist;
+    // 現在探索中のアドレスを次のアドレスに更新
+    // いずれNULLになるので絶対にwhikeは抜ける
+    nowlist = nowlist->next;
+
+  }
+
+  // 右側がfreeであれば拡張済み、そうでなければそのままのmetadataをfreelistとして追加
   my_add_to_free_list(metadata);
+
 }
 
 // This is called at the end of each challenge.
